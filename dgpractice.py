00071ea61bc0c8e9a43600135447632a7234c016
tania dumbrava <tinel.hajkova@gmail.com>
2016-10-27 17:13:25 +0200

    My Django Girls app, first commit

---
+++ dgpractice.py
@@ -0,0 +1,45 @@
+def draft():
+    name = 'tania'
+
+    if name == 'tania':
+       print('hey tania')
+    elif name == 'tania':
+       print('hey tania')
+    else:
+        print('hey anonymous')
+
+
+    volume=500
+
+    if volume < 30:
+        print("It's quiet")
+    elif 30 >= volume < 60:
+        print("It's nice for background")
+    elif 60 <= volume < 80:
+        print("Perfect, let's party")
+    elif 80 <= volume <120:
+        print("Too loud")
+    else:
+        print("Crazy")
+
+def draft():
+        print('ahoooooj')
+        print('jak se mas?')
+
+def ahoj(name):
+    print('ahoj' + name + '!')
+ahoj('lucik')
+
+def hi():
+    print('hi there!')
+    print('how are you?')
+hi()
+
+def hi(name):
+    if name == 'nick':
+        print('hi nick')
+    elif name == 'maria':
+        print('hi maria')
+    else:
+        print('hi anonymous')
+hi('dora')        
